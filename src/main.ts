dbf23b2a99ea4fd55a5268ba384fee7ecef70ad2
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2021-02-17 15:00:38 +0100

    Include bootstrap and custom stylesheet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../public/style.css'
 
 createApp(App).use(store).mount('#app')
