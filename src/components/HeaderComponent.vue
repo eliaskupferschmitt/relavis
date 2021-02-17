43a029baf31a4483caf21fece7449372e346ca8f
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2021-02-17 15:02:56 +0100

    Display header with search bar

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <ul class="navbar-nav ml-auto mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active mt-2">
+        <label class="input-group">
+          <input type="search" class="form-control rounded" placeholder="Search for person..." v-model="searchBar"/>
+          <button type="button" class="btn btn-outline-primary" @click="search">Search</button>
+        </label>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+  },
+  methods: {
+    search () {
+      if (this.$data.searchBar.trim() !== '') {
+        this.$store.commit('search', { name: this.$data.searchBar })
+      }
+    }
+  }
+})
+export default class HeaderComponent extends Vue {
+  data () {
+    return {
+      searchBar: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
