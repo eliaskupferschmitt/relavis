0ee42de2a6d5fdb5ba0278aee5661f7f5a7e8619
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2021-02-17 15:02:33 +0100

    Display sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,13 +1,8 @@
 <template>
     <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Start Bootstrap </div>
+      <div class="sidebar-heading">relavis</div>
       <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+        <button class="list-group-item list-group-item-action bg-light" :key="i" v-for="(c, i) in content" @click="c.action()">{{c.title}}</button>
       </div>
     </div>
 </template>
@@ -15,64 +10,37 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 
+class SidebarObject {
+  readonly title: string
+  readonly action: Function
+
+  constructor (title: string, action?: Function) {
+    this.title = title
+    this.action = action !== undefined ? action : () => {
+      console.log(`triggered default action for ${title}`)
+    }
+  }
+}
+
 @Options({
   props: {
   }
 })
 export default class SidebarComponent extends Vue {
+  data () {
+    return {
+      content: [
+        new SidebarObject('Lorem', () => alert('This is an example action!')),
+        new SidebarObject('Ipsum'),
+        new SidebarObject('dolor'),
+        new SidebarObject('sit'),
+        new SidebarObject('amet')
+      ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/*!
- * Start Bootstrap - Simple Sidebar (https://startbootstrap.com/template/simple-sidebar)
- * Copyright 2013-2020 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
- */
-
-#wrapper {
-  overflow-x: hidden;
-}
-
-#sidebar-wrapper {
-  min-height: 100vh;
-  margin-left: -15rem;
-  -webkit-transition: margin .25s ease-out;
-  -moz-transition: margin .25s ease-out;
-  -o-transition: margin .25s ease-out;
-  transition: margin .25s ease-out;
-}
-
-#sidebar-wrapper .sidebar-heading {
-  padding: 0.875rem 1.25rem;
-  font-size: 1.2rem;
-}
-
-#sidebar-wrapper .list-group {
-  width: 15rem;
-}
-
-#page-content-wrapper {
-  min-width: 100vw;
-}
-
-#wrapper.toggled #sidebar-wrapper {
-  margin-left: 0;
-}
-
-@media (min-width: 768px) {
-  #sidebar-wrapper {
-    margin-left: 0;
-  }
-
-  #page-content-wrapper {
-    min-width: 0;
-    width: 100%;
-  }
-
-  #wrapper.toggled #sidebar-wrapper {
-    margin-left: -15rem;
-  }
-}
 </style>
